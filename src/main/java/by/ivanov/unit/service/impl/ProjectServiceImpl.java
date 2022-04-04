c7129334c2534119db981bc58bd70032d214a47a
ivan <i.s.ivanov2307@gmail.com>
2022-04-04 21:17:22 +0300

    show short name general contractor in projects list

--- src/main/java/by/ivanov/unit/service/impl/ProjectServiceImpl.java
+++ src/main/java/by/ivanov/unit/service/impl/ProjectServiceImpl.java
@@ -5,13 +5,14 @@ import by.ivanov.unit.repository.ProjectRepository;
 import by.ivanov.unit.service.ProjectService;
 import by.ivanov.unit.service.dto.ProjectDTO;
 import by.ivanov.unit.service.mapper.ProjectMapper;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * Service Implementation for managing {@link Project}.
@@ -58,11 +59,12 @@ public class ProjectServiceImpl implements ProjectService {
     @Transactional(readOnly = true)
     public Page<ProjectDTO> findAll(Pageable pageable) {
         log.debug("Request to get all Projects");
-        return projectRepository.findAll(pageable).map(projectMapper::toDto);
+        return projectRepository.findAll(pageable).map(projectMapper::toDtoGenContractorName);
     }
 
     public Page<ProjectDTO> findAllWithEagerRelationships(Pageable pageable) {
-        return projectRepository.findAllWithEagerRelationships(pageable).map(projectMapper::toDto);
+        return projectRepository.findAllWithEagerRelationships(pageable)
+	        .map(projectMapper::toDtoGenContractorName);
     }
 
     @Override
--- src/main/java/by/ivanov/unit/service/mapper/ProjectMapper.java
+++ src/main/java/by/ivanov/unit/service/mapper/ProjectMapper.java
@@ -2,14 +2,18 @@ package by.ivanov.unit.service.mapper;
 
 import by.ivanov.unit.domain.Project;
 import by.ivanov.unit.service.dto.ProjectDTO;
-import java.util.Set;
-import org.mapstruct.*;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 /**
  * Mapper for the entity {@link Project} and its DTO {@link ProjectDTO}.
  */
 @Mapper(componentModel = "spring", uses = { CompanyMapper.class })
 public interface ProjectMapper extends EntityMapper<ProjectDTO, Project> {
+
+	@Named("generalContractorId")
     @Mapping(target = "generalContractor", source = "generalContractor", qualifiedByName = "id")
     @Mapping(target = "subContractors", source = "subContractors", qualifiedByName = "idSet")
     ProjectDTO toDto(Project s);
@@ -22,4 +26,9 @@ public interface ProjectMapper extends EntityMapper<ProjectDTO, Project> {
     @Mapping(target = "id", source = "id")
     @Mapping(target = "name", source = "name")
     ProjectDTO toDtoName(Project project);
+
+	@Mapping(target = "generalContractor", source = "generalContractor",
+		qualifiedByName = "shortName")
+	@Mapping(target = "subContractors", source = "subContractors", qualifiedByName = "idSet")
+	ProjectDTO toDtoGenContractorName(Project project);
 }
