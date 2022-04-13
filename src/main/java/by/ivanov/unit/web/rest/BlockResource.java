7d170af035ecf7827c465847cd44275afb0c340f
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2022-04-13 16:15:55 +0300

    update imports

--- src/main/java/by/ivanov/unit/web/rest/BlockResource.java
+++ src/main/java/by/ivanov/unit/web/rest/BlockResource.java
@@ -1,18 +1,12 @@
 package by.ivanov.unit.web.rest;
 
 import by.ivanov.unit.repository.BlockRepository;
+import by.ivanov.unit.security.AuthoritiesConstants;
 import by.ivanov.unit.service.BlockQueryService;
 import by.ivanov.unit.service.BlockService;
 import by.ivanov.unit.service.criteria.BlockCriteria;
 import by.ivanov.unit.service.dto.BlockDTO;
 import by.ivanov.unit.web.rest.errors.BadRequestAlertException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,6 +20,14 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
 import tech.jhipster.web.util.PaginationUtil;
 import tech.jhipster.web.util.ResponseUtil;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * REST controller for managing {@link by.ivanov.unit.domain.Block}.
@@ -85,14 +87,13 @@ public class BlockResource {
 	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated blockDTO,
 	 * or with status {@code 400 (Bad Request)} if the blockDTO is not valid,
 	 * or with status {@code 500 (Internal Server Error)} if the blockDTO couldn't be updated.
-	 * @throws URISyntaxException if the Location URI syntax is incorrect.
 	 */
 	@PutMapping("/blocks/{id}")
 	@PreAuthorize("hasRole(\"" + AuthoritiesConstants.ADMIN + "\")")
 	public ResponseEntity<BlockDTO> updateBlock(
 		@PathVariable(value = "id", required = false) final Long id,
 		@Valid @RequestBody BlockDTO blockDTO
-	) throws URISyntaxException {
+	) {
 		log.debug("REST request to update Block : {}, {}", id, blockDTO);
 		if (blockDTO.getId() == null) {
 			throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
@@ -123,14 +124,13 @@ public class BlockResource {
 	 * or with status {@code 400 (Bad Request)} if the blockDTO is not valid,
 	 * or with status {@code 404 (Not Found)} if the blockDTO is not found,
 	 * or with status {@code 500 (Internal Server Error)} if the blockDTO couldn't be updated.
-	 * @throws URISyntaxException if the Location URI syntax is incorrect.
 	 */
 	@PatchMapping(value = "/blocks/{id}", consumes = { "application/json", "application/merge-patch+json" })
 	@PreAuthorize("hasRole(\"" + AuthoritiesConstants.ADMIN + "\")")
 	public ResponseEntity<BlockDTO> partialUpdateBlock(
 		@PathVariable(value = "id", required = false) final Long id,
 		@NotNull @RequestBody BlockDTO blockDTO
-	) throws URISyntaxException {
+	) {
 		log.debug("REST request to partial update Block partially : {}, {}", id, blockDTO);
 		if (blockDTO.getId() == null) {
 			throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
