b9a55e7c2d2578b58cbb216fade7f459f95397a6
DESKTOP-U2527QI\mnpz <h2h.eredivisie@gmail.com>
2022-04-15 12:30:48 +0300

    update view punch-lists

--- src/main/webapp/app/entities/punch-list/list/punch-list.component.ts
+++ src/main/webapp/app/entities/punch-list/list/punch-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest } from 'rxjs';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { combineLatest, Subscription } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { IPunchList } from '../punch-list.model';
@@ -9,109 +9,172 @@ import { IPunchList } from '../punch-list.model';
 import { ASC, DESC, ITEMS_PER_PAGE, SORT } from 'app/config/pagination.constants';
 import { PunchListService } from '../service/punch-list.service';
 import { PunchListDeleteDialogComponent } from '../delete/punch-list-delete-dialog.component';
+import { DropdownDataService } from '../../../shared/dropdown-data.service';
 
 @Component({
-  selector: 'jhi-punch-list',
-  templateUrl: './punch-list.component.html',
+	selector: 'jhi-punch-list',
+	templateUrl: './punch-list.component.html'
 })
-export class PunchListComponent implements OnInit {
-  punchLists?: IPunchList[];
-  isLoading = false;
-  totalItems = 0;
-  itemsPerPage = ITEMS_PER_PAGE;
-  page?: number;
-  predicate!: string;
-  ascending!: boolean;
-  ngbPaginationPage = 1;
+export class PunchListComponent implements OnInit, OnDestroy {
+	punchLists?: IPunchList[];
+	isLoading = false;
+	totalItems = 0;
+	itemsPerPage = ITEMS_PER_PAGE;
+	page?: number;
+	predicate!: string;
+	ascending!: boolean;
+	ngbPaginationPage = 1;
 
-  constructor(
-    protected punchListService: PunchListService,
-    protected activatedRoute: ActivatedRoute,
-    protected router: Router,
-    protected modalService: NgbModal
-  ) {}
+	filterNumber?: number;
+	filterName?: string;
+	filterDescription?: string;
+	filterProjectId?: number;
 
-  loadPage(page?: number, dontNavigate?: boolean): void {
-    this.isLoading = true;
-    const pageToLoad: number = page ?? this.page ?? 1;
+	projectNotifierSubscription: Subscription = this.dropdownDataService.projectNotifier.subscribe((projectId) => {
+		this.filterProjectId = projectId;
+		this.loadPage(1);
+	});
 
-    this.punchListService
-      .query({
-        page: pageToLoad - 1,
-        size: this.itemsPerPage,
-        sort: this.sort(),
-      })
-      .subscribe({
-        next: (res: HttpResponse<IPunchList[]>) => {
-          this.isLoading = false;
-          this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate);
-        },
-        error: () => {
-          this.isLoading = false;
-          this.onError();
-        },
-      });
-  }
+	constructor(
+		protected punchListService: PunchListService,
+		protected activatedRoute: ActivatedRoute,
+		protected router: Router,
+		protected modalService: NgbModal,
+		protected dropdownDataService: DropdownDataService
+	) {
+	}
 
-  ngOnInit(): void {
-    this.handleNavigation();
-  }
+	loadPage(page?: number, dontNavigate?: boolean): void {
+		this.isLoading = true;
+		const pageToLoad: number = page ?? this.page ?? 1;
 
-  trackId(_index: number, item: IPunchList): number {
-    return item.id!;
-  }
+		const req = {};
+		Object.assign(req, { page: pageToLoad - 1 });
+		Object.assign(req, { size: this.itemsPerPage });
+		Object.assign(req, { sort: this.sort() });
 
-  delete(punchList: IPunchList): void {
-    const modalRef = this.modalService.open(PunchListDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
-    modalRef.componentInstance.punchList = punchList;
-    // unsubscribe not needed because closed completes on modal close
-    modalRef.closed.subscribe(reason => {
-      if (reason === 'deleted') {
-        this.loadPage();
-      }
-    });
-  }
+		this.addFiltersParam(req);
 
-  protected sort(): string[] {
-    const result = [this.predicate + ',' + (this.ascending ? ASC : DESC)];
-    if (this.predicate !== 'id') {
-      result.push('id');
-    }
-    return result;
-  }
+		this.punchListService.query(req).subscribe({
+			next: (res: HttpResponse<IPunchList[]>) => {
+				this.isLoading = false;
+				this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate);
+			},
+			error: () => {
+				this.isLoading = false;
+				this.onError();
+			}
+		});
+	}
 
-  protected handleNavigation(): void {
-    combineLatest([this.activatedRoute.data, this.activatedRoute.queryParamMap]).subscribe(([data, params]) => {
-      const page = params.get('page');
-      const pageNumber = +(page ?? 1);
-      const sort = (params.get(SORT) ?? data['defaultSort']).split(',');
-      const predicate = sort[0];
-      const ascending = sort[1] === ASC;
-      if (pageNumber !== this.page || predicate !== this.predicate || ascending !== this.ascending) {
-        this.predicate = predicate;
-        this.ascending = ascending;
-        this.loadPage(pageNumber, true);
-      }
-    });
-  }
+	ngOnInit(): void {
+		this.handleNavigation();
+	}
 
-  protected onSuccess(data: IPunchList[] | null, headers: HttpHeaders, page: number, navigate: boolean): void {
-    this.totalItems = Number(headers.get('X-Total-Count'));
-    this.page = page;
-    if (navigate) {
-      this.router.navigate(['/punch-list'], {
-        queryParams: {
-          page: this.page,
-          size: this.itemsPerPage,
-          sort: this.predicate + ',' + (this.ascending ? ASC : DESC),
-        },
-      });
-    }
-    this.punchLists = data ?? [];
-    this.ngbPaginationPage = this.page;
-  }
+	trackId(_index: number, item: IPunchList): number {
+		return item.id!;
+	}
 
-  protected onError(): void {
-    this.ngbPaginationPage = this.page ?? 1;
-  }
+	delete(punchList: IPunchList): void {
+		const modalRef = this.modalService.open(PunchListDeleteDialogComponent, {
+			size: 'lg',
+			backdrop: 'static'
+		});
+		modalRef.componentInstance.punchList = punchList;
+		// unsubscribe not needed because closed completes on modal close
+		modalRef.closed.subscribe(reason => {
+			if (reason === 'deleted') {
+				this.loadPage();
+			}
+		});
+	}
+
+	clearFilter(): void {
+		this.filterNumber = undefined;
+		this.filterName = undefined;
+		this.filterDescription = undefined;
+		this.filterProjectId = undefined;
+		this.loadPage();
+	}
+
+	ngOnDestroy(): void {
+		this.projectNotifierSubscription.unsubscribe();
+	}
+
+	protected sort(): string[] {
+		const result = [this.predicate + ',' + (this.ascending ? ASC : DESC)];
+		if (this.predicate !== 'id') {
+			result.push('id');
+		}
+		return result;
+	}
+
+	protected handleNavigation(): void {
+		combineLatest([this.activatedRoute.data, this.activatedRoute.queryParamMap]).subscribe(([data, params]) => {
+			const page = params.get('page');
+			const pageNumber = +(page ?? 1);
+			const sort = (params.get(SORT) ?? data['defaultSort']).split(',');
+			const predicate = sort[0];
+			const ascending = sort[1] === ASC;
+
+			this.getFilterParamsFromRoute(params);
+
+			if (pageNumber !== this.page || predicate !== this.predicate || ascending !== this.ascending) {
+				this.predicate = predicate;
+				this.ascending = ascending;
+				this.loadPage(pageNumber, true);
+			}
+		});
+	}
+
+	protected onSuccess(data: IPunchList[] | null, headers: HttpHeaders, page: number, navigate: boolean): void {
+		this.totalItems = Number(headers.get('X-Total-Count'));
+		this.page = page;
+		if (navigate) {
+			this.router.navigate(['/punch-list'], {
+				queryParams: this.prepareQueryParam(),
+			});
+		}
+		this.punchLists = data ?? [];
+		this.ngbPaginationPage = this.page;
+	}
+
+	protected onError(): void {
+		this.ngbPaginationPage = this.page ?? 1;
+	}
+
+	protected prepareQueryParam(): {} {
+		const param = {};
+		Object.assign(param, { page: this.page });
+		Object.assign(param, { size: this.itemsPerPage });
+		Object.assign(param, { sort: this.predicate + ',' + (this.ascending ? ASC : DESC) });
+		this.addFiltersParam(param);
+		return param;
+	}
+
+	private addFiltersParam(param: {}): void {
+		if (this.filterNumber) {
+			Object.assign(param, { 'number.equals': this.filterNumber });
+		}
+		if (this.filterName) {
+			Object.assign(param, { 'name.contains': this.filterName });
+		}
+		if (this.filterDescription) {
+			Object.assign(param, { 'description.contains': this.filterDescription });
+		}
+		if (this.filterProjectId) {
+			Object.assign(param, { 'projectId.equals': this.filterProjectId });
+		}
+	}
+
+	private getFilterParamsFromRoute(params: ParamMap): void {
+		const number = params.get('number.equals');
+		this.filterNumber = number !== null ? +number : undefined;
+
+		this.filterName = params.get('name.contains') ?? undefined;
+		this.filterDescription = params.get('description.contains') ?? undefined;
+
+		const projectId = params.get('projectId.equals');
+		this.filterProjectId = projectId !== null ? +projectId : undefined;
+	}
 }
--- src/main/webapp/app/entities/punch-list/route/punch-list-routing.module.ts
+++ src/main/webapp/app/entities/punch-list/route/punch-list-routing.module.ts
@@ -9,43 +9,47 @@ import { PunchListRoutingResolveService } from './punch-list-routing-resolve.ser
 import { Authority } from '../../../config/authority.constants';
 
 const punchListRoute: Routes = [
-  {
-    path: '',
-    component: PunchListComponent,
-    data: {
-      defaultSort: 'id,asc',
-    },
-  },
-  {
-    path: ':id/view',
-    component: PunchListDetailComponent,
-    resolve: {
-      punchList: PunchListRoutingResolveService,
-    },
-  },
-  {
-    path: 'new',
-    component: PunchListUpdateComponent,
-    resolve: {
-      punchList: PunchListRoutingResolveService,
-    },
-	  data: {
-		  authorities: [Authority.ADMIN, Authority.CUSTOMER, Authority.COMMISSIONER],
-	  },
-    canActivate: [UserRouteAccessService],
-  },
-  {
-    path: ':id/edit',
-    component: PunchListUpdateComponent,
-    resolve: {
-      punchList: PunchListRoutingResolveService,
-    },
-    canActivate: [UserRouteAccessService],
-  },
+	{
+		path: '',
+		component: PunchListComponent,
+		data: {
+			defaultSort: 'id,asc'
+		}
+	},
+	{
+		path: ':id/view',
+		component: PunchListDetailComponent,
+		resolve: {
+			punchList: PunchListRoutingResolveService
+		}
+	},
+	{
+		path: 'new',
+		component: PunchListUpdateComponent,
+		resolve: {
+			punchList: PunchListRoutingResolveService
+		},
+		data: {
+			authorities: [Authority.ADMIN, Authority.CUSTOMER, Authority.COMMISSIONER]
+		},
+		canActivate: [UserRouteAccessService]
+	},
+	{
+		path: ':id/edit',
+		component: PunchListUpdateComponent,
+		resolve: {
+			punchList: PunchListRoutingResolveService
+		},
+		data: {
+			authorities: [Authority.ADMIN, Authority.CUSTOMER, Authority.COMMISSIONER]
+		},
+		canActivate: [UserRouteAccessService]
+	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(punchListRoute)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(punchListRoute)],
+	exports: [RouterModule]
 })
-export class PunchListRoutingModule {}
+export class PunchListRoutingModule {
+}
